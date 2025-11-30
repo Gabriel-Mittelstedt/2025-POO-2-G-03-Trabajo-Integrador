7aac51389e2d2b81519f15d2bb23e873b2b39c02
Axel Joan Dos Santos <axeldossantos141@gmail.com>
2025-11-30 20:05:34 -0300

    commit fixeo

--- integrador/src/main/java/com/unam/integrador/model/Factura.java
+++ integrador/src/main/java/com/unam/integrador/model/Factura.java
@@ -25,7 +25,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import lombok.AccessLevel;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
